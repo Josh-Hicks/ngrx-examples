3114f56d084c08941250df4ebc371225c3a2b0d7
Tom Em <tomem.email@gmail.com>
2020-10-12 00:41:39 +0200

    chore: Unit testing without TestBed
    - upgrade to Angular 10
    - add unit tests without TestBed

---
+++ src/app/containers/sign-out-page/sign-out-page.component.2.spec.ts
@@ -0,0 +1,28 @@
+import { Router } from '@angular/router';
+import { userSignIn } from 'src/app/store/app.actions';
+import { Mock, mockService } from 'src/test-utils';
+import { SignOutPageComponent } from './sign-out-page.component';
+import { SignOutPageFacade } from './sign-out-page.facade';
+
+describe('SignOutPageComponent2', () => {
+  let component: SignOutPageComponent;
+
+  let router: Mock<Router>;
+  let facade: Mock<SignOutPageFacade>;
+
+  beforeEach(() => {
+    router = mockService(Router);
+    facade = mockService(SignOutPageFacade);
+
+    component = new SignOutPageComponent(facade, router);
+  });
+
+  it('should dispatch UserSignIn and navigate to home onLogin', () => {
+    // given & when
+    component.login();
+
+    // then
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/home');
+    expect(facade.dispatch).toHaveBeenCalledWith(userSignIn());
+  });
+});
